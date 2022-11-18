8b90e3de36843467460d2d1aa8b1a3b07c257d10
Bùi Thị Lan Anh PH21324 <anhbtlph21324@fpt.edu.vn>
2022-11-18 17:29:27 +0700

    commit1_Lananh

---
+++ QuanLyTapHoa/src/Utilities/DBConnection.java
@@ -0,0 +1,29 @@
+package Utilities;
+
+import java.sql.*;
+
+public class DBConnection {
+    private static Connection conn;
+    public static Connection getConnection(){
+        if(conn == null){
+            try {
+                Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                String dbUser = "sa", dbPass = "Aa@123456",
+                        dbUrl = "jdbc:sqlserver://localhost:1433;"
+                            +"databaseName=QuanLyTapHoa;"
+                            +"encrypt=true;trustServerCertificate=true;sslProtocol=TLSv1.2";
+                conn = DriverManager.getConnection(dbUrl, dbUser, dbPass);
+                System.out.println("Kết nối thành công");
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            
+        }
+        return conn;
+    }
+    public static void main(String[] args) {
+        getConnection();
+    }
+}
