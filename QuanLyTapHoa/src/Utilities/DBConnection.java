c026d28463f282ee65c16073fd66c7cad2f312ea
NT-Hieu <117371824+NT-Hieu@users.noreply.github.com>
2022-11-21 13:17:33 +0700

    Commit_5_thanhhieu_Login

--- QuanLyTapHoa/src/Utilities/DBConnection.java
+++ QuanLyTapHoa/src/Utilities/DBConnection.java
@@ -1,29 +1,48 @@
 package Utilities;
 
+import com.microsoft.sqlserver.jdbc.SQLServerDriver;
 import java.sql.*;
 
 public class DBConnection {
-    private static Connection conn;
-    public static Connection getConnection(){
-        if(conn == null){
-            try {
-                Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-                String dbUser = "sa", dbPass = "Aa@123456",
-                        dbUrl = "jdbc:sqlserver://localhost:1433;"
-                            +"databaseName=QuanLyCuaHangTapHoa;"
-                            +"encrypt=true;trustServerCertificate=true;sslProtocol=TLSv1.2";
-                conn = DriverManager.getConnection(dbUrl, dbUser, dbPass);
-                System.out.println("Kết nối thành công");
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            } catch (SQLException e) {
-                e.printStackTrace();
+     private static final String USERNAME = "sa";
+    private static final String PASSWORD = "123";
+    private static final String SERVER_NAME = "DESKTOP-CSTQHQ6\\SQLEXPRESS";
+    private static final String PORT = "1433";
+    private static final String DATABASE_NAME = "QuanLyCuaHangTapHoa";
+    private static final boolean USING_SSL = true;
+
+    private static String CONNECT_STRING;
+
+    static {
+        try {
+            DriverManager.registerDriver(new SQLServerDriver());
+            //Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+            StringBuilder connectStringBuilder = new StringBuilder();
+            connectStringBuilder.append("jdbc:sqlserver://")
+                    .append(SERVER_NAME).append(":").append(PORT).append(";")
+                    .append("databaseName=").append(DATABASE_NAME).append(";")
+                    .append("user=").append(USERNAME).append(";")
+                    .append("password=").append(PASSWORD).append(";");
+            if (USING_SSL) {
+                connectStringBuilder.append("encrypt=true;trustServerCertificate=true;");
             }
-            
+            CONNECT_STRING = connectStringBuilder.toString();
+            System.out.println("Connect String co dang: " + CONNECT_STRING);
+            System.out.println("Ket noi thanh cong");
+        } catch (Exception ex) {
         }
-        return conn;
     }
-    public static void main(String[] args) {
-        getConnection();
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(CONNECT_STRING);
     }
+
+    public static void main(String[] args) throws SQLException {
+        Connection conn = getConnection();
+        String dbpn = conn.getMetaData().getDatabaseProductName();
+        System.out.println(dbpn);
+
+    }
+
 }
